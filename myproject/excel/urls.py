b82efde1efa1bcb82bf56a25d77e06de78fec0c5
Jay1425 <jayraychura13@gmail.com>
2026-04-10 17:34:26 +0530

    OpsFlow

--- myproject/excel/urls.py
+++ myproject/excel/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.data_list, name='excel_home'),
     path('upload/', views.upload_excel, name='upload_excel'),
     path('data/', views.data_list, name='data_list'),
 ]
